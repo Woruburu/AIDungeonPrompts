c24a6193a1fe8f15a26c3c8b9f29d5dc4527c003
Woruburu <G2gNFuKaoXkqR2Qz@protonmail.com>
2020-11-21 23:11:29 +0000

    change date.now to date.utcnow

--- AIDungeonPrompts.Domain/Entities/Abstract/BaseDomainEntity.cs
+++ AIDungeonPrompts.Domain/Entities/Abstract/BaseDomainEntity.cs
@@ -4,7 +4,7 @@ namespace AIDungeonPrompts.Domain.Entities.Abstract
 {
 	public abstract class BaseDomainEntity
 	{
-		public DateTime DateCreated { get; set; } = DateTime.Now;
+		public DateTime DateCreated { get; set; } = DateTime.UtcNow;
 		public DateTime? DateEdited { get; set; }
 		public int Id { get; set; }
 	}
