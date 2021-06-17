45c6d4134bc0002ed659e328270e8af91db1938e
woruburu <G2gNFuKaoXkqR2Qz@protonmail.com>
2021-06-17 18:57:57 +0100

    add nai scenario export

---
+++ AIDungeonPromptsWeb/Models/NovelAi/NovelAiContextConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AIDungeonPrompts.Web.Models.NovelAi
+{
+	public class NovelAiContextConfig
+	{
+		[JsonPropertyName("prefix")]
+		public string Prefix { get; set; } = string.Empty;
+
+		[JsonPropertyName("suffix")]
+		public string Suffix { get; set; } = string.Empty;
+
+		[JsonPropertyName("tokenBudget")]
+		public int TokenBudget { get; set; }
+
+		[JsonPropertyName("reservedTokens")]
+		public int ReservedTokens { get; set; }
+
+		[JsonPropertyName("budgetPriority")]
+		public int BudgetPriority { get; set; }
+
+		[JsonPropertyName("trimDirection")]
+		public string TrimDirection { get; set; } = string.Empty;
+
+		[JsonPropertyName("insertionType")]
+		public string InsertionType { get; set; } = string.Empty;
+
+		[JsonPropertyName("insertionPosition")]
+		public int InsertionPosition { get; set; }
+	}
+}
---
+++ AIDungeonPromptsWeb/Models/NovelAi/NovelAiLorebook.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace AIDungeonPrompts.Web.Models.NovelAi
+{
+	public class NovelAiLorebook
+	{
+		[JsonPropertyName("lorebookVersion")]
+		public int LorebookVersion => 1;
+
+		[JsonPropertyName("entries")]
+		public List<NovelAiLorebookEntry> LorebookEntries { get; set; } = new();
+	}
+}
