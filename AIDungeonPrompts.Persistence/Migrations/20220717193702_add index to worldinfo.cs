a886b14ae1252bb71249ec09ada2b083efec0171
woruburu <G2gNFuKaoXkqR2Qz@protonmail.com>
2022-07-17 20:57:34 +0100

    add index to world info prompt id

---
+++ AIDungeonPrompts.Persistence/Migrations/20220717193702_add index to worldinfo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AIDungeonPrompts.Persistence.Migrations
+{
+    public partial class addindextoworldinfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
